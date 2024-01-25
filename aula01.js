b0d6b9458c9e9115ab7c868a3ab392a6c047e550
daniel araujo <daniellealuau@gmail.com>
2024-01-25 15:38:29 -0300

    ok

--- aula01.js
+++ aula01.js
@@ -1,36 +1,48 @@
+// este bloco de código declara duas variáveis, exposta e naoExposta, mas apenas exposta é acessível fora do bloco devido ao uso de var.
 if (true) {
-    var exposta = "exposta"
-    let naoExposta = "naoExposta"
+    var exposta = "exposta"; // exposta é declarada com var, tornando-a escopo de função
+    let naoExposta = "naoExposta"; // naoExposta é declarada com let, tornando-a escopo de bloco
 }
 
-console.log(exposta, naoExposta)
+// tentar logar exposta e naoExposta fora do bloco resultará em um erro para naoExposta.
+console.log(exposta, naoExposta); // exposta será logada, mas naoExposta resultará em um erro
 
-for (let i = 0; i<=10; i++) {
-    var j = i;
-    console.log(i)
+// este loop loga os valores de i de 0 a 10.
+for (let i = 0; i <= 10; i++) {
+    var j = i; // j é declarado com var, tornando-o escopo de função e acessível fora do loop
+    console.log(i); // loga o valor atual de i
 }
 
-console.log(j)
+// o valor de j é acessível fora do loop e será logado aqui.
+console.log(j);
 
-let/const variavel = 10
-variavel = 20
+// o código a seguir tenta reatribuir um valor a uma variável constante, o que resultará em um erro.
+let const variavel = 10;
+variavel = 20; // esta linha resultará em um erro, pois variavel é declarada com const
 
-console.log(variavel)
+// logando o valor de variavel, mas não chegará a este ponto devido ao erro acima.
+console.log(variavel);
 
-const arr = [1,2,3]
-arr.push(4)
-console.log(arr)
-arr.pop()
-console.log(arr)
+// arrays e objetos declarados com const ainda podem ser modificados, desde que a referência permaneça a mesma.
 
+// declarando um array constante e adicionando um novo elemento a ele.
+const arr = [1, 2, 3];
+arr.push(4);
+console.log(arr); // [1, 2, 3, 4]
+
+// removendo o último elemento do array.
+arr.pop();
+console.log(arr); // [1, 2, 3]
+
+// declarando um objeto constante e adicionando uma nova propriedade a ele.
 const obj = {
     a: 1,
     b: 2
-}
+};
 
-obj.c = 3
+obj.c = 3;
+console.log(obj); // { a: 1, b: 2, c: 3 }
 
-console.log(obj)
-
-delete obj.c
-console.log(obj)
+// excluindo uma propriedade do objeto.
+delete obj.c;
+console.log(obj); // { a: 1, b: 2 }
---
+++ aula02.js
@@ -0,0 +1,24 @@
+// const minhaF = function () {
+//     console.log("algiuma coisa")
+// }
+//
+// minhaF();
+//
+// function minhaFe () {
+//     console.log("algiuma coisa")
+// }
+//
+// minhaFe()
+
+// const funcao = () => "uma expressao de funmcao"
+//
+// funcao()
+// console.log(funcao());
+// nao funcionou
+
+const obj = {
+    thisLocal: ()=> {
+
+    }
+}
+
