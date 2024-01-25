4fe996a2521fb8976df481943fdda193ff67e71b
daniel araujo <daniellealuau@gmail.com>
2024-01-24 21:17:04 -0300

    primeira aula

---
+++ aula01.js
@@ -0,0 +1,36 @@
+if (true) {
+    var exposta = "exposta"
+    let naoExposta = "naoExposta"
+}
+
+console.log(exposta, naoExposta)
+
+for (let i = 0; i<=10; i++) {
+    var j = i;
+    console.log(i)
+}
+
+console.log(j)
+
+let/const variavel = 10
+variavel = 20
+
+console.log(variavel)
+
+const arr = [1,2,3]
+arr.push(4)
+console.log(arr)
+arr.pop()
+console.log(arr)
+
+const obj = {
+    a: 1,
+    b: 2
+}
+
+obj.c = 3
+
+console.log(obj)
+
+delete obj.c
+console.log(obj)
