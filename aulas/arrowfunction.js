44233f80136e01c84ab60d39ad1cc01803a4cb99
daniel araujo <daniellealuau@gmail.com>
2024-01-26 20:29:14 -0300

    function

--- aulas/arrowfunction.js
+++ aulas/arrowfunction.js
@@ -1,6 +1,6 @@
 // declaração de uma função anônima usando a palavra-chave const
 const minhaF = function () {
-    console.log("alguma coisa");
+    console.log("1 alguma coisa");
 }
 
 // chamada da função
@@ -8,14 +8,23 @@ minhaF();
 
 // declaração de uma função nomeada usando a palavra-chave function
 function minhaFe() {
-    console.log("alguma coisa");
+    console.log("2 alguma coisa");
 }
 
 // chamada da função
 minhaFe();
 
+
+const divisao = (a, b) => {
+    return a / b;
+};
+
+
+let resultadoDivisao = divisao(10, 2);
+console.log(resultadoDivisao); 
+
 // declaração de uma expressão de função usando a arrow function
-const funcao = () => "uma expressao de funcao";
+const funcao = () => "4 uma expressao de funcao";
 
 // chamada da função (a expressão de função retorna um valor)
 funcao();
@@ -24,14 +33,6 @@ funcao();
 // observe que o código a seguir pode não funcionar como esperado devido a um erro de sintaxe.
 
 // tentativa de reatribuir um valor a uma constante
-const obj = {
-    thisLocal: () => {
-
-    }
-};
-
-// para corrigir, você precisa atribuir uma função diferente à propriedade thisLocal do objeto
-// exemplo:
 
 const obj = {
     // método dentro do objeto
@@ -39,14 +40,16 @@ const obj = {
         // IIFE (Função Invocada Imediatamente) usando uma função de seta (arrow function)
         const thisLocal = (() => {
             // loga o valor de 'this' dentro da função de seta (que herda 'this' do método thisLocal)
-            console.log(this);
+            console.log("this", this);
         })(); // IIFE é invocada imediatamente devido aos parênteses no final
 
         // loga o resultado da IIFE (que é undefined, pois a IIFE não retorna nada)
-        console.log(thisLocal);
+        console.log("7", thisLocal);
     }
 };
 
 // chama o método thisLocal do objeto 'obj'
 obj.thisLocal();
+
+
 
