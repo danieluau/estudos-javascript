09eb5e99e8111cb423cfa633906081ab7c94cfda
daniel araujo <daniellealuau@gmail.com>
2024-03-03 10:06:23 -0300

    ok

---
+++ introducao/mat/mat.js
@@ -0,0 +1,23 @@
+let num1 = 30
+
+console.log(30 + 40)
+console.log(num1 * 40)
+console.log(num1 - 20)
+console.log(num1 / 30)
+
+
+console.log(num1 % 12)
+
+console.log(++num1)
+
+console.log(--num1)
+
+
+console.log(Math.PI)
+
+let num3 = 16
+
+console.log(Math.sqrt(num3))
+
+console.log(Math.tan(num3))
+
